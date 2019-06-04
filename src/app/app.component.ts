e8cbcca9ff88d9711b303f0bbee4c8f2c816542e
Omar Kan <omar@veriguide.org>
2019-06-04 14:58:33 +0800

    feat(SwaggerUI): Add SwaggerUI to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SwaggerUI from 'swagger-ui';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  // styleUrls: ['../../node_modules/swagger-ui/dist/swagger-ui.css', './app.component.scss']
+  // styleUrls: ['/node_modules/swagger-ui/dist/swagger-ui.css', './app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'myapp';
+  constructor() {
+  }
+
+  ngOnInit () {
+    SwaggerUI({
+      dom_id: '#myDomId',
+      // url: 'http://localhost:4200/assets/swagger.json' || 'http://localhost:4200/assets/swagger-demo.yaml'
+      url: 'http://localhost:4200/assets/swagger-gensim.yaml'
+    })
+  }
 }
