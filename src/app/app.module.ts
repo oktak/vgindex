b069d1532a9a111dc37caa20facc293a4020f213
Omar Kan <omar@veriguide.org>
2019-06-19 10:12:05 +0800

    feat(UI): Remove Angular Material, use PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-
-import { NgxMdModule } from 'ngx-md';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FunctionListComponent } from './function-list/function-list.component';
-import { SwaggerListComponent } from './swagger-list/swagger-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import {NgModule} from '@angular/core';
-import {A11yModule} from '@angular/cdk/a11y';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {PortalModule} from '@angular/cdk/portal';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 
-import {ReadmeContComponent} from './readme-cont/readme-cont.component';
-import {HttpClientModule} from '@angular/common/http';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {HomeComponent} from './home/home.component';
 import {CardModule} from 'primeng/card';
+import {SidebarModule} from 'primeng/sidebar';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+
+import {NgxMdModule} from 'ngx-md';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {AppComponent} from './app.component';
+import {FunctionListComponent} from './function-list/function-list.component';
+import {SwaggerListComponent} from './swagger-list/swagger-list.component';
+import {ReadmeContComponent} from './readme-cont/readme-cont.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -32,21 +27,16 @@ import {CardModule} from 'primeng/card';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule,
     NgxMdModule.forRoot(),
     FlexLayoutModule,
-    BrowserAnimationsModule,
     CardModule,
-    A11yModule,
-    CdkStepperModule,
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,
-    PortalModule,
-    ScrollingModule,
+    SidebarModule,
+    ToolbarModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
